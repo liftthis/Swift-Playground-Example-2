15e2db48b76434396bb853d2fa7ed8732d95799e
Fred Martinez <frederickmartinez@hotmail.com>
2015-09-24 13:50:05 -0700

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var decNum: Int = 140
+var binary = String(decNum, radix: 2)
+
+var test = "The binary equiviant of \(decNum) is \(binary)."
+
